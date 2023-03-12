321aaf07f96df172fd02e467b8d526b70155adc5
vhnguyen2003 <vonguyenbest@gmail.com>
2023-03-12 13:46:42 +0700

    add product feature

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const globalErrorHandler = require("./middlewares/globalErrorHandler");
 
 //IMPORT ROUTES
 const categoryRouter = require("./routes/categoryRoutes");
+const productRouter = require("./routes/productRoutes");
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
@@ -15,6 +16,7 @@ app.use(express.json({ limit: "50mb" }));
 
 //ROUTER
 app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(globalErrorHandler);
 module.exports = app;
