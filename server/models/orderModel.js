648ba0ef500c9cd6738849789315b03ce92885dc
Newt Tran <tranngoctan910@gmail.com>
2023-03-19 15:48:11 +0700

    update required

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -5,7 +5,7 @@ const orderSchema = new Schema({
     customer: {
         type: Schema.Types.ObjectId,
         ref: "User",
-        required: [true, "Order must have Customer ID"],
+        required: [true, "Vui lòng nhập ID của khách hàng"],
     },
     staff: {
         type: Schema.Types.ObjectId,
@@ -16,29 +16,29 @@ const orderSchema = new Schema({
             product: {
                 type: Schema.Types.ObjectId,
                 ref: "Product",
-                required: [true, "Order Detail must have Product ID"],
+                required: [true, "Vui lòng nhập ID của sản phẩm"],
             },
             quantity: {
                 type: Number,
-                required: [true, "Order Detail must have Quantity"],
+                required: [true, "Vui lòng nhập số lượng của sản phẩm"],
             },
         },
     ],
     total: {
         type: Number,
-        required: [true, "Order Detail must have Total"],
+        required: true,
     },
     status: {
         type: String,
-        required: [true, "Order Detail must have Status"],
+        required: [true, "Vui lòng nhập trạng thái của đơn hàng"],
     },
     payment_status: {
         type: String,
-        required: [true, "Order Detail must have Payment Status"],
+        required: [true, "Vui lòng nhập trạng thái thanh toán của đơn hàng"],
     },
     payment_method: {
         type: String,
-        required: [true, "Order Detail must have Payment Method"],
+        required: [true, "Vui lòng nhập phương thức thanh toán của đơn hàng"],
     },
     createdAt: {
         type: Date,
