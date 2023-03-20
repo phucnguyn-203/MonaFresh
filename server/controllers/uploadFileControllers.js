0544906270fbfce9eb966e9cc9d0bcfbe26c3409
Newt Tran <tranngoctan910@gmail.com>
2023-03-20 14:05:19 +0700

    upload to cloudinary

--- server/controllers/uploadFileControllers.js
+++ server/controllers/uploadFileControllers.js
@@ -1,8 +1,12 @@
 const catchAsync = require("../utils/catchAsync");
+const cloudinary = require("../utils/cloudinary");
 
 exports.uploadFile = catchAsync(async (req, res) => {
-    console.log(req.file.path);
+   
+    const upload= await cloudinary.uploader.upload(req.file.path);
+    console.log(upload);
     res.status(200).json({
         status: "success",
+        URL:upload.url,
     });
 });
