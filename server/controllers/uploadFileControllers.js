369d2c91e659ab32769ed3aa642d284269203756
Nguyễn Hoàng Phúc <87885153+phucnguyn-203@users.noreply.github.com>
2023-03-23 08:30:33 +0700

    add config for server

--- server/controllers/uploadFileControllers.js
+++ server/controllers/uploadFileControllers.js
@@ -1,5 +1,5 @@
 const catchAsync = require("../utils/catchAsync");
-const cloudinary = require("../utils/cloudinary");
+const cloudinary = require("../config/cloudinary");
 
 exports.uploadFile = catchAsync(async (req, res) => {
     const upload = await cloudinary.uploader.upload(req.file.path);
