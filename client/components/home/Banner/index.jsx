2915cc7fb3a8e4913ddcc6438e56ec5187f30cd7
Anhphung14 <phungmiss@gmail.com>
2023-04-07 10:09:01 +0700

    bg

--- client/components/home/Banner/index.jsx
+++ client/components/home/Banner/index.jsx
@@ -1,35 +1,32 @@
 import Link from "next/link";
 import styles from "./styles.module.css";
 
-import banner from "@/public/assets/img/bgHome.jpg";
+import banner from "@/public/assets/img/bgHomtest.png";
 
 export default function Banner() {
-    return (
-        <div
-            style={{ backgroundImage: `url(${banner.src})` }}
-            className={`${styles.banner}`}
-        >
-            <div className="w-full bottom-32 absolute flex flex-col text-white justify-center items-center uppercase z-10">
-                <div>
-                    <h2
-                        className="tracking-widest m-4 px-4 text-center font-bold"
-                        style={{ textShadow: "2px 6px 8px #292929" }}
-                    >
-                        <span className="text-4xl">tìm mua</span>{" "}
-                        <b className="text-6xl">Thực phẩm sạch</b>{" "}
-                        <span className="text-4xl "> từ</span>
-                        <span className="flex-col flex "></span>
-                        <b className="text-6xl  ">Nhà cung cấp uy tín</b>
-                        <span className="text-4xl"> tại đây</span>
-                    </h2>
-                </div>
-                <Link
-                    href="/shop"
-                    className="text-lg bg-[#6abd45] px-8 py-3 mt-3 rounded-lg uppercase hover:bg-lime-600 transition-all"
-                >
-                    Mua ngay
-                </Link>
-            </div>
+  return (
+    <div
+      style={{ backgroundImage: `url(${banner.src})` }}
+      className={`${styles.banner}`}>
+      <div className="w-full top-[30%] left-[30%] translate-x-[-30%] translate-y-[-30%] absolute flex flex-col text-white justify-center items-center uppercase z-10">
+        <div>
+          <h2
+            className="tracking-widest m-4 px-4 text-center font-bold"
+            style={{ textShadow: "2px 6px 8px #292929" }}>
+            <span className="text-4xl">tìm mua</span>{" "}
+            <b className="text-6xl">Thực phẩm sạch</b>{" "}
+            <span className="text-4xl "> từ</span>
+            <span className="flex-col flex "></span>
+            <b className="text-6xl  ">Nhà cung cấp uy tín</b>
+            <span className="text-4xl"> tại đây</span>
+          </h2>
         </div>
-    );
+        <Link
+          href="/shop"
+          className="text-lg bg-[#6abd45] px-8 py-3 mt-3 rounded-lg uppercase hover:bg-lime-600 transition-all">
+          Mua ngay
+        </Link>
+      </div>
+    </div>
+  );
 }
