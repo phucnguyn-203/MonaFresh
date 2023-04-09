d6cf61e6d5aa7adad7a9e07ca41a8a71780a620b
vhnguyen2003 <vonguyenbest@gmail.com>
2023-04-09 13:40:41 +0700

    create product detail

--- client/components/Product/ProductItem/index.jsx
+++ client/components/Product/ProductItem/index.jsx
@@ -2,33 +2,31 @@ import Link from "next/link";
 import formatCurrency from "@/utils/formatCurrency";
 import styles from "./styles.module.css";
 
-export default function ProductItem({ thumbnail, name, price }) {
-    return (
-        <Link
-            href="/"
-            className={`${styles.product} shadow-lg hover:shadow-gray-500`}
-        >
-            <div className="overflow-hidden">
-                <div
-                    style={{ backgroundImage: `url(${thumbnail})` }}
-                    className={`${styles.productThumnail} pt-[100%] bg-no-repeat bg-contain bg-center transition-transform ease-linear duration-300`}
-                ></div>
-            </div>
-            <div className="text-center mt-[10px] mx-[10px] mb-[20px]">
-                <p className="text-[#1C1C1C] hover:text-[#6abd45] font-semibold truncate">
-                    {name}
-                </p>
+export default function ProductItem({ id, thumbnail, name, price }) {
+  return (
+    <Link
+      href={`/shop/${id}`}
+      className={`${styles.product} shadow-lg hover:shadow-gray-500`}
+    >
+      <div className="overflow-hidden">
+        <div
+          style={{ backgroundImage: `url(${thumbnail})` }}
+          className={`${styles.productThumnail} pt-[100%] bg-no-repeat bg-contain bg-center transition-transform ease-linear duration-300`}
+        ></div>
+      </div>
+      <div className="text-center mt-[10px] mx-[10px] mb-[20px]">
+        <p className="text-[#1C1C1C] hover:text-[#6abd45] font-semibold truncate">
+          {name}
+        </p>
 
-                <div className="text-[#6abd45] mb-[10px]">
-                    {formatCurrency(price)}
-                </div>
-                <div>
-                    <button className="bg-primary text-white px-5 py-3 text-xs hover:bg-lime-600 rounded-md">
-                        THÊM VÀO GIỎ HÀNG
-                    </button>
-                </div>
-            </div>
-        </Link>
-    );
+        <div className="text-[#6abd45] mb-[10px]">{formatCurrency(price)}</div>
+        <div>
+          <button className="bg-primary text-white px-5 py-3 text-xs hover:bg-lime-600 rounded-md">
+            THÊM VÀO GIỎ HÀNG
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
 }
 //className={`flex flex-col items-center min-h-[210px] max-h-[350px] w-full border-gray-100 border border-solid shadow-md hover:shadow-2xl mx-[5px]  ${styles.scaleImg}`}
--- client/components/shop/Products/index.jsx
+++ client/components/shop/Products/index.jsx
@@ -3,21 +3,22 @@ import Pagination from "@/components/shared/Pagination";
 
 import { products } from "@/api/data";
 export default function Products() {
-    return (
-        <div className="w-3/4 ">
-            <div className="grid grid-cols-4 gap-4 mb-[30px]">
-                {products.map(({ id, name, price, thumbnail }) => (
-                    <ProductItem
-                        key={id}
-                        name={name}
-                        price={price}
-                        thumbnail={thumbnail}
-                    />
-                ))}
-            </div>
-            <div>
-                <Pagination />
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-3/4 ">
+      <div className="grid grid-cols-4 gap-4 mb-[30px]">
+        {products.map(({ id, name, price, thumbnail }) => (
+          <ProductItem
+            key={id}
+            id={id}
+            name={name}
+            price={price}
+            thumbnail={thumbnail}
+          />
+        ))}
+      </div>
+      <div>
+        <Pagination />
+      </div>
+    </div>
+  );
 }
