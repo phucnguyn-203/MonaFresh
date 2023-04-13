07a13d22b239effa772acf9e250af99aac89c698
Anhphung14 <phungmiss@gmail.com>
2023-04-13 14:19:17 +0700

    hiddenUploadSetting

--- admin/src/pages/setting/index.jsx
+++ admin/src/pages/setting/index.jsx
@@ -33,30 +33,35 @@ export default function Setting() {
                             </label>
                             <div className="col-span-8 sm:col-span-4 ">
                                 <div className="w-full text-center">
-                                    <div className="px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md cursor-pointer">
-                                        <input type="file" hidden />
-                                        <span className="mx-auto flex justify-center">
-                                            <svg
-                                                stroke="currentColor"
-                                                fill="none"
-                                                stroke-width="2"
-                                                viewBox="0 0 24 24"
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                class="text-3xl text-green-500"
-                                                height="1em"
-                                                width="1em"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <polyline points="16 16 12 12 8 16"></polyline>
-                                                <line x1="12" y1="12" x2="12" y2="21"></line>
-                                                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
-                                                <polyline points="16 16 12 12 8 16"></polyline>
-                                            </svg>
-                                        </span>
-                                        <p className="text-sm mt-2">Tải ảnh lên tại đây</p>
-                                        <em className="text-xs text-gray-400">(Chỉ nhận file ảnh *.jpeg và *.png)</em>
-                                    </div>
+                                    <input type="file" hidden id="file" accept="image/*" />
+                                    <label htmlFor="file">
+                                        <div className="px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md cursor-pointer">
+                                            <span className="mx-auto flex justify-center">
+                                                <svg
+                                                    stroke="currentColor"
+                                                    fill="none"
+                                                    stroke-width="2"
+                                                    viewBox="0 0 24 24"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    class="text-3xl text-green-500"
+                                                    height="1em"
+                                                    width="1em"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <polyline points="16 16 12 12 8 16"></polyline>
+                                                    <line x1="12" y1="12" x2="12" y2="21"></line>
+                                                    <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                                    <polyline points="16 16 12 12 8 16"></polyline>
+                                                </svg>
+                                            </span>
+                                            <p className="text-sm mt-2">Tải ảnh lên tại đây</p>
+                                            <em className="text-xs text-gray-400">
+                                                (Chỉ nhận file ảnh *.jpeg và *.png)
+                                            </em>
+                                        </div>
+                                    </label>
+
                                     <aside className="flex flex-row flex-wrap mt-4">
                                         <img
                                             src={avt}
