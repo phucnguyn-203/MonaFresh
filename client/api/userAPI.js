8aa422c7a94ac557392458790a9e05ec892afdd4
Nguyễn Hoàng Phúc <87885153+phucnguyn-203@users.noreply.github.com>
2023-05-17 03:12:47 +0700

    add authenticate for client

--- client/api/userAPI.js
+++ client/api/userAPI.js
@@ -1,10 +1,19 @@
 import axios from "./axios";
 
 const userAPI = {
+  login: async (email, password) => {
+    const url = "users/login";
+    return axios.post(url, { email, password });
+  },
   signUp: async (data) => {
     const { name, email, phone, password, passwordConfirm } = data;
     const url = "/users/signup";
     await axios.post(url, { name, email, phone, password, passwordConfirm });
+  },
+  checkIsLogin: async () => {
+    const url = "/users/check-login";
+    const response = await axios.get(url);
+    return response;
   },
   forgotPassword: async (data) => {
     const url = "/users/forgotPassword";
--- client/components/layout/DefaultLayout/index.jsx
+++ client/components/layout/DefaultLayout/index.jsx
@@ -1,14 +1,41 @@
+import React, { useState, useEffect } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
-
+import { useDispatch } from "react-redux";
+import { setUserSuccess, setUserFail } from "@/features/auth/authSlice";
+import userAPI from "@/api/userAPI";
+import Loading from "@/components/loading";
 import styles from "./styles.module.css";
 
 export default function DefaultLayout({ children }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkIsLogin = async () => {
+      try {
+        const response = await userAPI.checkIsLogin();
+        dispatch(setUserSuccess(response.data));
+      } catch {
+        dispatch(setUserFail);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    checkIsLogin();
+  }, []);
+
   return (
-    <div className={styles.app}>
-      <Header />
-      <main className={styles.content}>{children}</main>
-      <Footer />
-    </div>
+    <React.Fragment>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className={styles.app}>
+          <Header />
+          <main className={styles.content}>{children}</main>
+          <Footer />
+        </div>
+      )}
+    </React.Fragment>
   );
 }
