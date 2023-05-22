9a43086fcba00b761008aa0941ffb805d2e0bce5
Nguyễn Hoàng Phúc <87885153+phucnguyn-203@users.noreply.github.com>
2023-05-22 13:33:05 +0700

    add get all order

--- admin/src/api/orderAPI.js
+++ admin/src/api/orderAPI.js
@@ -1,6 +1,11 @@
 import axios from "./axios";
 
 const orderAPI = {
+  getAllOrder: async (params) => {
+    const url = "/orders";
+    const response = await axios.get(url, { params });
+    return response;
+  },
   getOrdersByUserId: async (userId) => {
     const url = `orders/user/${userId}`;
     const response = await axios.get(url);
