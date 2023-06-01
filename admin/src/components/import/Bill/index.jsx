73f417179b97c909f50bbbfb072578343c31f90f
Anhphung14 <phungmiss@gmail.com>
2023-06-02 00:26:10 +0700

    ImportBill

--- admin/src/components/import/Bill/index.jsx
+++ admin/src/components/import/Bill/index.jsx
@@ -59,9 +59,9 @@ export default function Bill({ close, invoice }) {
                         <div className="">Số lượng</div>
                       </td>
                       <td className="text-center py-2 px-3">
-                        <div className="">Đơn giá</div>
+                        <div className="">Giá nhập</div>
                       </td>
-                      <td className="text-right py-2 pr-3">
+                      <td className="text-center py-2 px-3">
                         <div className="">Thành tiền</div>
                       </td>
                     </tr>
@@ -79,7 +79,7 @@ export default function Bill({ close, invoice }) {
                         <td className="text-sm text-center">
                           <span>{item.price}</span>
                         </td>
-                        <td className="bg-white text-[red] font-normal  divide-y py-2 pr-3 text-right text-sm divide-gray-10 ">
+                        <td className="bg-white text-[red] font-normal  divide-y py-2 px-3 text-center text-sm divide-gray-10 ">
                           <span>{formatCurrency(item.price * item.quantity)}</span>
                         </td>
                       </tr>
@@ -88,7 +88,7 @@ export default function Bill({ close, invoice }) {
                       <td colSpan={3} className="text-left pl-3 py-2 text-sm font-semibold">
                         Tạm tính
                       </td>
-                      <td className="bg-white divide-y py-2 pr-3 text-right text-sm divide-gray-10 text-[red] font-semibold">
+                      <td className="bg-white divide-y py-2 px-3 text-center text-sm divide-gray-10 text-[red] font-semibold">
                         {formatCurrency(totalPayment())}
                       </td>
                     </tr>
@@ -96,7 +96,7 @@ export default function Bill({ close, invoice }) {
                       <td colSpan={3} className="text-left pl-3 py-2 text-sm font-semibold">
                         Tiền vận chuyển
                       </td>
-                      <td className="bg-white divide-y py-2 pr-3 text-right text-sm divide-gray-10 text-[red] font-semibold">
+                      <td className="bg-white divide-y py-2 px-3 text-center text-sm divide-gray-10 text-[red] font-semibold">
                         {formatCurrency(0)}
                       </td>
                     </tr>
@@ -104,7 +104,7 @@ export default function Bill({ close, invoice }) {
                       <td colSpan={3} className="text-left pl-3 py-2 text-sm font-semibold">
                         Tổng tiền
                       </td>
-                      <td className="bg-white divide-y py-2 pr-3 text-right text-sm divide-gray-10 text-[red] font-semibold">
+                      <td className="bg-white divide-y py-2 px-3 text-center text-sm divide-gray-10 text-[red] font-semibold">
                         {formatCurrency(totalPayment())}
                       </td>
                     </tr>
