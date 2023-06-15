60df66dd0005e8fc28808e4d928dad78c95fec5a
Nguyễn Hoàng Phúc <87885153+phucnguyn-203@users.noreply.github.com>
2023-06-15 20:58:17 +0700

    change config

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,8 +1,11 @@
 const sendToken = (res, { name, token, maxAge }) => {
     cookieOptions = {
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "strict",
     };
+    if (process.env.NODE_ENV === "production") {
+        cookieOptions.secure = true;
+    }
 
     res.cookie(name, token, { ...cookieOptions, maxAge });
 };
