4e8deac4b67cbaeca8ded87bcccb518355f66bd1
Nguyễn Hoàng Phúc <87885153+phucnguyn-203@users.noreply.github.com>
2023-06-15 14:59:22 +0700

    change config

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -5,7 +5,6 @@ const sendToken = (res, { name, token, maxAge }) => {
     };
     if (process.env.NODE_ENV === "production") {
         cookieOptions.secure = true;
-        cookieOptions.domain = "vercel.app";
     }
 
     res.cookie(name, token, { ...cookieOptions, maxAge });
